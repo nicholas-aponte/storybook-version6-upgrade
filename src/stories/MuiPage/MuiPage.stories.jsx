e5b698b3b926550cd43df5dc2c8c67807af691e6
Nicholas <87664225+nicholas-aponte@users.noreply.github.com>
2021-10-12 14:41:50 -0400

    fixed issues

--- src/stories/MuiPage/MuiPage.stories.jsx
+++ src/stories/MuiPage/MuiPage.stories.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import {getLists} from "./../../data/api"
 import { MuiPage } from "../MuiPage/MuiPage";
 import * as formStories from "../form/form.stories";
-<<<<<<< HEAD
 
  getLists()
 
@@ -11,12 +10,9 @@ let listOptions = []
 for (let i = 0; i < lists.length; i++){
   listOptions.push(lists[i]["listName"]);
 }
-console.log(listOptions)
-console.log(listOptions);
+
 const optionsArray = ["Dark", "Light", "Red", "Blue"]
 
-=======
->>>>>>> daee7f1b3a4047cc0575d96e467b23afcc8fcf3c
 
 export default {
   title: "Demo/MuiPage",
